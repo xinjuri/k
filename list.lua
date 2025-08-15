cbed47f67e858a73753f9ca4dc9a4ddc69f83afb
xinjuri <xinjuri@gmail.com>
2025-08-15 10:39:58 -0400

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [87039211657390] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/99%20Nights%20in%20the%20Forest.lua", -- [UPD] Arise Crossover
+  [87039211657390] = "https://raw.githubusercontent.com/xinjuri/k/refs/heads/main/cheat.lua", -- [UPD] Arise Crossover
 }
 
 return Games
