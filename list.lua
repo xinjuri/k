219f3afe4a1b7e57e81b34491b6fa5f3280a73a4
xinjuri <xinjuri@gmail.com>
2025-08-15 10:34:39 -0400

    Create list.lua

---
+++ list.lua
@@ -0,0 +1,5 @@
+local Games = {
+  [87039211657390] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/99%20Nights%20in%20the%20Forest.lua", -- [UPD] Arise Crossover
+}
+
+return Games
